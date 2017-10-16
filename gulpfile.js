e58498c4d19645bf584772c639849fd953b1b5e5
meowwwls <meowwwls@users.noreply.github.com>
2017-10-15 22:23:48 -0500

    Add cleanup gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const del = require('del');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 const babel = require('gulp-babel');
@@ -7,7 +8,9 @@ const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 
-gulp.task('css', () => {
+gulp.task('clean', () => del(['dist/**', '!dist'], { force: true }));
+
+gulp.task('css', ['clean'], () => {
   return gulp
     .src('src/**/*.css')
     .pipe(prefix())
@@ -17,7 +20,7 @@ gulp.task('css', () => {
     .pipe(reload({ stream: true }));
 });
 
-gulp.task('js', () => {
+gulp.task('js', ['clean'], () => {
   return gulp
     .src('src/**/*.js')
     .pipe(babel())
